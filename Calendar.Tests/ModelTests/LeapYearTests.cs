f1afff7041cc5a69fcc5e6c64e2d489d23706a8b
cass1618 <cass16180@gmail.com>
2021-05-04 09:43:54 -0700

    First test good fail
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.Tests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+    // Test method will go here
+  }
+}
